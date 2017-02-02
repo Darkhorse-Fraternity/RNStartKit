f2145cb64baf10d2124663d461ffae97c5d8f935
tonyYo <420156367@qq.com>
2017-02-02 14:30:55 +0800

    no message

--- ios/RNStartKitTests/RNStartKitTests.m
+++ ios/RNStartKitTests/RNStartKitTests.m
@@ -17,11 +17,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface jiuBuyTests : XCTestCase
+@interface RNStartKitTests : XCTestCase
 
 @end
 
-@implementation jiuBuyTests
+@implementation RNStartKitTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
